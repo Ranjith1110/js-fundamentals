9b1e358e7a75d648738a4a98a1f2ac7806901e1b
Ranjith <ranjithram878@gmail.com>
2024-11-22 19:53:30 +0530

    Updated

---
+++ Js Fundamentals/3-type-conversition.js
@@ -0,0 +1,49 @@
+let val;
+
+// Number to string
+val = String(555);
+val = String(5 + 5);
+
+// console.log(val);
+// console.log(typeof val);
+
+// Boolean to string
+val = String(true);
+val = String(false);
+
+// console.log(val);
+// console.log(typeof val);
+
+// Array to string
+val = String([1, 2, 3, 4]);
+
+// console.log(val);
+// console.log(typeof val);
+
+// String to Number
+val = Number("5");
+val = Number("5 + 5") // NaN Not a number
+
+// console.log(val);
+// console.log(typeof val);
+
+// Boolean to Number
+val = Number(true);
+val = Number (false);
+val = Number (null);
+val = Number ([1, 2, 3, 4]) // NaN Not a number
+val = Number ("Hello") // NaN Not a number
+
+val = parseInt("100.35");
+val = parseFloat("100.35");
+
+// console.log(val);
+// console.log(typeof val);
+
+
+
+
+
+
+
+
---
+++ Js Fundamentals/4-number.js
@@ -0,0 +1,17 @@
+
+const numb1 = 100;
+const numb2 = 200;
+
+let val;
+
+// Simple math with Number
+
+val = numb1 + numb2;
+val = numb1 - numb2;
+val = numb1 * numb2;
+val = numb1 / numb2;
+val = numb1 % numb2;
+
+// console.log(val);
+
+
