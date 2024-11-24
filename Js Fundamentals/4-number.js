8fe08f4d599cd41dff18d07efb763980b10ac4de
Ranjith <ranjithram878@gmail.com>
2024-11-24 13:16:36 +0530

    Updated

--- Js Fundamentals/4-number.js
+++ Js Fundamentals/4-number.js
@@ -1,4 +1,3 @@
-
 const numb1 = 100;
 const numb2 = 200;
 
@@ -14,4 +13,12 @@ val = numb1 % numb2;
 
 // console.log(val);
 
+// Math Object
 
+val = Math.PI;
+val = Math.round(2.3); // If the decimal is 0.5 or greater, the value is 3. Otherwise, if it's lower, the value is 2.
+val = Math.round(2.5);
+val = Math.ceil(2.4); // The output is 3 because the decimal 0.5 rounds up, just like how a ceiling in a house is always above us, so the number goes up too!
+val = Math.floor(2.9); // The output is 2 because the decimal is below 0.5, similar to how a floor is below us, so the number goes down.
+
+console.log(val);
