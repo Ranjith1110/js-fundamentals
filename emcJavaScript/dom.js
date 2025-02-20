a571f43d78f9bc0689332d708da8af4cfcaff953
Ranjith <ranjithram878@gmail.com>
2025-02-20 18:47:59 +0530

    Random Numbers is Pending

---
+++ emcJavaScript/dom.js
@@ -0,0 +1,24 @@
+// Date: 2021/05/17 
+/*
+var example = document.getElementById('title');
+example.innerText = 'Bye Bye World!';
+
+var num1 = document.getElementById('num1');
+var num2 = document.getElementById('num2');
+var show = document.getElementById('show');
+
+function add(){
+    var n1 = parseInt(num1.value);
+    var n2 = parseInt(num2.value);
+    var sum = n1 + n2;
+    show.innerText = 'The sum is: ' + sum;
+}
+*/
+
+var ram = Math.random();
+console.log(Math.floor(ram*10));
+
+/*
+var floor = Math.floor(ram*10);
+console.log(floor);
+*/
