8697729dab89504f0107f9ec6a1c499ac194fa3c
Ranjith <ranjithram878@gmail.com>
2025-02-22 12:54:22 +0530

    Task 1

--- emcJavaScript/dom.js
+++ emcJavaScript/dom.js
@@ -15,10 +15,43 @@ function add(){
 }
 */
 
-var ram = Math.random();
-console.log(Math.floor(ram*10));
+// var ram = Math.random();
+// console.log(Math.floor(ram*10)+1);
 
 /*
 var floor = Math.floor(ram*10);
 console.log(floor);
+*/
+
+/*
+var gessnumber = document.getElementById('gessnumber');
+var message = document.getElementById('message');
+var random = Math.floor(Math.random() * 10) + 1;
+var totalscore = 10;
+var scoreDisplay = document.getElementById('scoredisplay');
+console.log(random);    
+
+function guessNumber() {
+
+    var gess = parseInt(gessnumber.value);  
+    if (random === gess) {
+        message.innerText = 'You have guessed the correct number!';
+        alert('You have guessed the correct number!');
+        // console.log("Right");
+
+    } else {
+        totalscore -= 1;
+        scoreDisplay.innerText = 'Score is : ' + totalscore;
+        message.innerText = 'The number is incorrect!';
+        alert('The number is incorrect!');
+        // console.log("Wrong");
+    }
+}
+*/
+/*
+var boxColor = document.getElementById('colorchange');
+
+function changeColor() {
+    boxColor.style.backgroundColor = 'violet';
+}
 */
